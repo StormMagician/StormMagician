61b5f4eb4d9a30abcb649fba38dd03dee366bc36
StormMagician <49549587+StormMagician@users.noreply.github.com>
2019-04-17 15:43:25 +0800

    Update orgSplitStartListener.java

--- flowable-spring-boot/src/main/java/org/flowable/listeners/orgSplitStartListener.java
+++ flowable-spring-boot/src/main/java/org/flowable/listeners/orgSplitStartListener.java
@@ -1,7 +1,11 @@
 package org.flowable.listeners;
 
+import java.util.Date;
+
 import org.flowable.common.engine.api.delegate.Expression;
 import org.flowable.engine.delegate.TaskListener;
+import org.flowable.rocketmq.MQProducter;
+import org.flowable.rocketmq.TaskProperties;
 import org.flowable.task.service.delegate.DelegateTask;
 
 public class orgSplitStartListener implements TaskListener {
@@ -15,11 +19,25 @@ public class orgSplitStartListener implements TaskListener {
 	public void setFileSN(Expression fileSN) {
 		this.fileSN = fileSN;
 	}
-
+	
+	
+	
 	@Override
-	public void notify(DelegateTask delegateTask) {
+	public synchronized void notify(DelegateTask delegateTask) {
+		String fileSNStr = fileSN.getValue(delegateTask).toString();
+		TaskProperties taskProperties = new TaskProperties();
+		taskProperties.setTaskID(delegateTask.getId());
+		taskProperties.setArticleID(fileSNStr);
+		MQProducter mqProducter = new MQProducter();
+		try {
+			mqProducter.init();
+			mqProducter.sendMessage(taskProperties);
+			mqProducter.distroy();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println(delegateTask.getId());
-		System.out.println(fileSN.getValue(delegateTask).toString());
 	}
 
 }
