ee2c121a4d1da1d8af2e0a5fb02f6be3dc836664
StormMagician <49549587+StormMagician@users.noreply.github.com>
2019-04-17 15:43:38 +0800

    Update MQProducter.java

--- flowable-spring-boot/src/main/java/org/flowable/rocketmq/MQProducter.java
+++ flowable-spring-boot/src/main/java/org/flowable/rocketmq/MQProducter.java
@@ -1,9 +1,13 @@
 package org.flowable.rocketmq;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.log4j.Logger;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.common.message.Message;
@@ -15,19 +19,35 @@ import com.alibaba.fastjson.JSON;
 public class MQProducter extends MQClient{
 	private Logger logger = Logger.getLogger(getClass());
 	private DefaultMQProducer producter;
-
+	
 	@Override
 	public void init() throws Exception{
+		init(producterGroupName,nameServerAddr);
+	}
+
+	public void init(String groupNameOrAddr) throws Exception{
+		//一个参数，可能是生产者组名，可能是namesrv地址
+		if(groupNameOrAddr != null) {
+			String pattern = "(2(5[0-5]{1}|[0-4]\\d{1})|[0-1]?\\d{1,2})(\\.(2(5[0-5]{1}|[0-4]\\d{1})|[0-1]?\\d{1,2})){3}";
+	        Pattern r = Pattern.compile(pattern);
+	        Matcher m = r.matcher(groupNameOrAddr.substring(0, groupNameOrAddr.indexOf(":")));
+	        if(m.matches()) this.nameServerAddr = groupNameOrAddr;
+	        else this.producterGroupName = groupNameOrAddr;
+		}
+		init(producterGroupName,nameServerAddr);
+	}
+	
+	public void init(String producterGroupName,String nameServerAddr) throws Exception{
 		logger.info("开始启动消息生产者服务。。。");
 		producter = new DefaultMQProducer(producterGroupName);
 		producter.setNamesrvAddr(nameServerAddr);
 		producter.start();
 		logger.info("消息生产者服务启动成功。");
 	}
-
+	
 	@Override
 	public void distroy(){
-		logger.info("开始改关闭消息生产者服务。。。");
+		logger.info("开始关闭消息生产者服务。。。");
 		producter.shutdown();
 		logger.info("消息生产者服务已关闭。");
 	}
@@ -56,21 +76,32 @@ public class MQProducter extends MQClient{
 		System.out.println(result);
 		return result;
 	}
-	
-	public SendResult sendMessage(TaskProperties taskProperties,String tags){
+	public SendResult sendMessage(TaskProperties taskProperties,String topicName,String tags){
 		Message msg 		= null;
 		SendResult result 	= null;
 		try {
 			msg = new Message(topicName,tags,
 					JSON.toJSONString(taskProperties).getBytes());
+			//岗位名称+任务序号作为消息的唯一标识
+			String keys = taskProperties.getPostname()+taskProperties.getTaskID();
+			msg.setKeys(keys);
 			result = this.getProducter().send(msg);
 			if(result.getSendStatus()!=SendStatus.SEND_OK)
 				throw new Exception("消息发送失败！");
+			/*this.getProducter().send(msg, new SendCallback() {
+				@Override
+				public void onSuccess(SendResult sendResult) {
+					logger.info("消息发送成功"+sendResult);
+				}
+				
+				@Override
+				public void onException(Throwable e) {
+					logger.info(e.toString());
+				}
+			});*/
 		} catch (MQClientException e) {
 			e.printStackTrace();
 		} catch (RemotingException e) {
-			e.printStackTrace();
-		} catch (MQBrokerException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -78,6 +109,7 @@ public class MQProducter extends MQClient{
 		System.out.println(result);
 		return result;
 	}
+	
 	
 	
 }
