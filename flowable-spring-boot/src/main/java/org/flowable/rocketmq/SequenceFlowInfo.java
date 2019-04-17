ea4b09ce925ae395fde9f6d65a9e9cde05e18748
StormMagician <49549587+StormMagician@users.noreply.github.com>
2019-04-17 15:43:53 +0800

    Create SequenceFlowInfo.java

---
+++ flowable-spring-boot/src/main/java/org/flowable/rocketmq/SequenceFlowInfo.java
@@ -0,0 +1,20 @@
+package org.flowable.rocketmq;
+
+public class SequenceFlowInfo {
+	private String postName;
+	private String taskNature;
+	
+	public String getPostName() {
+		return postName;
+	}
+	public void setPostName(String postName) {
+		this.postName = postName;
+	}
+	public String getTaskNature() {
+		return taskNature;
+	}
+	public void setTaskNature(String taskNature) {
+		this.taskNature = taskNature;
+	}
+	
+}
